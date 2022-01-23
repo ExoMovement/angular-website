60650b27c47d6a0696ab8be50dbeef881be79c05
Sean McNamee <sm-exo-movement.com>
2022-01-23 13:07:51 -0500

    initialize page

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -4,10 +4,11 @@ import { AppPage } from './app.po';
 describe('workspace-project App', () => {
   let page: AppPage;
 
-  /*
+  
   beforeEach(() => {
     page = new AppPage();
   });
+  /*
 
   it('should display welcome message', async () => {
     await page.navigateTo();
